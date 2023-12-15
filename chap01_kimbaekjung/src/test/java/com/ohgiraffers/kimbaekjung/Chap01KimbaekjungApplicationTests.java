c149a67cba2bf058f99618ef1acf9c9f5eafe5f8
kucu-try <baekfive@gmail.com>
2023-12-15 18:13:26 +0900

    first commit

---
+++ chap01_kimbaekjung/src/test/java/com/ohgiraffers/kimbaekjung/Chap01KimbaekjungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.kimbaekjung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01KimbaekjungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
